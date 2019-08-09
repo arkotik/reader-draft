383cc0695e6ea80231bb3f50fac1518b1ad28a1a
arkotik <ar4i.666.13@gmail.com>
2019-08-09 10:13:24 +0300

    fixed calculating char sized

--- main.js
+++ main.js
@@ -15,6 +15,26 @@ function replaceEmptyLines(el) {
   el.innerHTML = el.innerHTML.replace(/<\/?empty-line>/gm, '<br>');
 }
 
+function getCharSize(code, spacing = 0) {
+  const char = String.fromCharCode(code);
+  const el = document.createElement('p');
+  el.style.setProperty('opacity', '0');
+  el.style.setProperty('display', 'block');
+  el.style.setProperty('width', 'fit-content');
+  el.style.setProperty('text-indent', '0');
+  document.querySelector('.text').appendChild(el);
+  el.innerText = char;
+  let size = el.getBoundingClientRect().width;
+  if (size === 0) {
+    const testChar = String.fromCharCode(5);
+    el.innerText = testChar;
+    const testSize = el.getBoundingClientRect().width;
+    el.innerText = testChar + char + testChar;
+    size = el.getBoundingClientRect().width - (2 * (testSize + spacing));
+  }
+  return size;
+}
+
 function calcSizes() {
   const el = document.createElement('p');
   el.style.setProperty('opacity', '0');
@@ -23,12 +43,12 @@ function calcSizes() {
   el.style.setProperty('text-indent', '0');
   document.querySelector('.text').appendChild(el);
   const map = {};
-  for (let code = 0; code <= 10000; code++) {
+  for (let code = 0; code <= 1300; code++) {
     el.innerText = String.fromCharCode(code);
     map[code] = el.getBoundingClientRect().width;
-    // if (code === 128) {
-    //   code = 'А'.charCodeAt(0) - 1;
-    // }
+    if (code === 128) {
+      code = 'А'.charCodeAt(0) - 1;
+    }
   }
   el.innerText = String.fromCharCode(5, 5);
   let letterSpacing = el.getBoundingClientRect().width - (2 * map[5]);
@@ -42,7 +62,15 @@ function calcAbzLength(abz) {
   let length = 0;
   const len = abz.length;
   for (let i = 0; i < len; i++) {
-    length += SIZES.map[abz.charCodeAt(i)] + SIZES.letterSpacing;
+    const code = abz.charCodeAt(i);
+    if (undefined === SIZES.map[code]) {
+      SIZES.map[code] = getCharSize(code, SIZES.letterSpacing);
+    }
+    length += SIZES.map[code] + SIZES.letterSpacing;
   }
   return length;
+}
+
+function calcAbzLines(text = '') {
+  return Math.ceil((calcAbzLength(text) + INDENT_SIZE) / width);
 }
